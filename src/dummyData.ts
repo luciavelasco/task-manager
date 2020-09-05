26bb2ecadd77e2993b76ba891e8dfc693f9bccc2
Lucia Velasco <luciakvelasco@gmail.com>
2020-09-05 22:51:50 +0100

    Refactor dummy data to inject priority and status
    This is because priority is very important to the f/e view. Status is less important and should probably be cut out.

--- src/dummyData.ts
+++ src/dummyData.ts
@@ -30,12 +30,12 @@ export enum StatusEnum {
   Assigned = 'Assigned'
 }
 
-const dummyTask: ITask = {
+export const dummyTask = (priority: PriorityEnum, status: StatusEnum): ITask => ({
   abxTaskId: 123,
   organisationTaskId: 321,
   organisationId: 3,
-  priority: PriorityEnum.LOW,
-  taskStatus: StatusEnum.InProgress,
+  priority: priority,
+  taskStatus: status,
   assignedto: 'AAA11AA',
   timestamp: new Date(),
   latitude: -150.321,
@@ -46,8 +46,15 @@ const dummyTask: ITask = {
     'collaborative thinking to further the overall value proposition. Organically grow ' +
     'the holistic world view of disruptive innovation via workplace diversity and empowerment.'
 
-}
-export const tasks = [ dummyTask, dummyTask, dummyTask, dummyTask, dummyTask, dummyTask, dummyTask ]
+})
+export const tasks = [
+  dummyTask(PriorityEnum.LOW, StatusEnum.InProgress),
+  dummyTask(PriorityEnum.HIGH, StatusEnum.Assigned),
+  dummyTask(PriorityEnum.MEDIUM, StatusEnum.Assigned),
+  dummyTask(PriorityEnum.HIGH, StatusEnum.InProgress),
+  dummyTask(PriorityEnum.LOW, StatusEnum.Complete),
+  dummyTask(PriorityEnum.LOW, StatusEnum.Complete),
+  dummyTask(PriorityEnum.MEDIUM, StatusEnum.InProgress) ]
 
 export interface IResponse<T = any> {
   status: number,
@@ -75,14 +82,14 @@ export const getTasksByCaller = (organisationId: number, callerId: number, acces
 
 // tasks/organisations/{organisationId}/{callerId}/latest
 export const getLatestTaskByCaller = (organisationId: number, callerId: number, accessId: number): IResponse<ITask> =>
-  response(200, dummyTask) // GET
+  response(200, dummyTask(PriorityEnum.MEDIUM, StatusEnum.InProgress)) // GET
 
 // tasks/{abxTaskId}/
 export const deleteTask = (abxTaskId: number): IResponse =>
   response(204) // DELETE
 
 export const getTask = (abxTaskId: number): IResponse<ITask> =>
-  response(200, dummyTask) // GET
+  response(200, dummyTask(PriorityEnum.MEDIUM, StatusEnum.InProgress)) // GET
 
 export const updateTask = (abxTaskId: number, task: ITask) =>
   response(201) // PUT
