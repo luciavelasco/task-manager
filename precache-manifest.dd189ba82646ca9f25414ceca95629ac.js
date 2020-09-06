d8f9cff32c8e2a5707752174f7a29c14c70a925d
Lucia Velasco <luciakvelasco@gmail.com>
2020-09-06 20:32:53 +0100

    Updates

---
+++ precache-manifest.dd189ba82646ca9f25414ceca95629ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e968766b441aab17f8d592767558d62",
+    "url": "/task-manager/index.html"
+  },
+  {
+    "revision": "9e8fa6bab9da44a78f0c",
+    "url": "/task-manager/static/css/2.63343480.chunk.css"
+  },
+  {
+    "revision": "f049673b4617e3c005f4",
+    "url": "/task-manager/static/css/main.3054824a.chunk.css"
+  },
+  {
+    "revision": "9e8fa6bab9da44a78f0c",
+    "url": "/task-manager/static/js/2.87ec7179.chunk.js"
+  },
+  {
+    "revision": "c87c79c859d06cbebfb50d49aa5fa4fe",
+    "url": "/task-manager/static/js/2.87ec7179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f049673b4617e3c005f4",
+    "url": "/task-manager/static/js/main.376fa1e7.chunk.js"
+  },
+  {
+    "revision": "d65e3eaf925f2942af3c",
+    "url": "/task-manager/static/js/runtime-main.57b90438.js"
+  }
+]);
